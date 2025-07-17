d8e8645ac366e33468481fc3ca3a27653b8427c3
perkowskikrzysio@gmail.com <perkowskikrzysio@gmail.com>
2025-07-17 16:56:50 +0200

    Update PerformanceTest: reduce data provider size from 100 to 20 for improved test efficiency

--- src/test/java/docker_test/PerformanceTest.java
+++ src/test/java/docker_test/PerformanceTest.java
@@ -10,8 +10,8 @@ public class PerformanceTest extends BaseTest {
 
     @DataProvider(name = "test-data", parallel = true)
     public Object[][] createData() {
-        Object[][] data = new Object[100][1];
-        for (int i = 0; i < 100; i++) {
+        Object[][] data = new Object[20][1];
+        for (int i = 0; i < 20; i++) {
             data[i][0] = i + 1;
         }
         return data;
